70c63b4183b4055bec09a477496ad0c9f275d83f
Peter Kordel <pkordel@gmail.com>
2017-11-16 14:11:17 +0700

    Parse top image in a better way

--- lib/image_attributes_parser.rb
+++ lib/image_attributes_parser.rb
@@ -4,16 +4,13 @@ ImagePageVO = Struct.new(:image_id, :image_page_id, :position,
 class ImageAttributesParser
   def self.parse(params)
     params_array = params.sort
-    top_image_page_id = if params_array.last.present? &&
-                           params_array.last[-1].key?('top_image')
-                          params_array.pop[-1]['top_image']
-                        end
 
     params_array.each_with_object([]) do |e, ary|
       position = e.first
-      image_id, image_page_id, caption =
-        e.last.values_at('id', 'image_page_id', 'caption')
-      top_image = image_page_id == top_image_page_id
+      image_id, image_page_id, caption, top_image_page_id =
+        e.last.values_at('id', 'image_page_id', 'caption', 'top_image')
+      top_image =
+        top_image_page_id.present? && top_image_page_id == image_page_id
       ary << ImagePageVO.new(image_id, image_page_id, position,
                              top_image, caption)
     end
